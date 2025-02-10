6307e7fcd82352d96d8dfce7acac3660a27e0372
GROWLL28 <102754478+GROWLL28@users.noreply.github.com>
2025-02-10 16:55:18 -0500

    __init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,3 +1,10 @@
 # db/__init__.py
 from .session import SessionLocal, engine, get_db
 from .base import Base
+
+# Make sure all models are imported
+from models.user import User
+from models.document import Document
+
+# Initialize models
+Base.metadata.create_all(bind=engine)
