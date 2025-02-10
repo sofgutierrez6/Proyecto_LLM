bf030cc506083ff491ed94b038aa10f43d8d4698
GROWLL28 <102754478+GROWLL28@users.noreply.github.com>
2025-02-10 16:53:29 -0500

    __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,3 +3,8 @@ from fastapi import APIRouter
 from .users import router as users_router
 from .documents import router as documents_router
 from .auth import router as auth_router
+
+api_router = APIRouter()
+api_router.include_router(users_router, prefix="/users", tags=["users"])
+api_router.include_router(documents_router, prefix="/documents", tags=["documents"])
+api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
