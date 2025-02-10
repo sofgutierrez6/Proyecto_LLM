b71fede8b623c76ef29c96bee6045702299be411
GROWLL28 <102754478+GROWLL28@users.noreply.github.com>
2025-02-10 16:42:16 -0500

    base.py

---
+++ app/db/base.py
@@ -0,0 +1,19 @@
+# db/base.py
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# db/session.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from core.config import settings
+
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
