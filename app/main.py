205a696d685fc3fc0df82c21724421fe7cdcb739
GROWLL28 <102754478+GROWLL28@users.noreply.github.com>
2025-02-10 16:48:15 -0500

    main.py

---
+++ app/main.py
@@ -0,0 +1,25 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api import auth, documents, users
+from core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+# Set up CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routers
+app.include_router(auth.router, prefix=settings.API_V1_STR, tags=["auth"])
+app.include_router(users.router, prefix=settings.API_V1_STR, tags=["users"])
+app.include_router(documents.router, prefix=settings.API_V1_STR, tags=["documents"])
