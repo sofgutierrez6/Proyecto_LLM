7a71919cc77f62fcc2945280445accdfebd812bc
GROWLL28 <102754478+GROWLL28@users.noreply.github.com>
2025-02-10 16:44:40 -0500

    user.py

---
+++ app/schemas/user.py
@@ -0,0 +1,18 @@
+# schemas/user.py
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: Optional[str] = None
+
+class UserCreate(UserBase):
+    password: str
+
+class UserInDB(UserBase):
+    id: int
+    created_at: datetime
+    
+    class Config:
+        from_attributes = True
