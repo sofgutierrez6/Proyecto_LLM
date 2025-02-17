a172bff05b4aef25d0cf8be5ac41cb3bd4ad8eb7
Megaomega99 <125285112+Megaomega99@users.noreply.github.com>
2025-02-17 22:56:24 +0000

    Actualización de archivos vacíos

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api import api_router  # Importa el router principal
 from core.config import settings
+import psycopg2
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -21,3 +22,23 @@ app.add_middleware(
 
 # Incluye el router principal
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+try:
+    connection = psycopg2.connect(
+        user="tu_usuario",
+        password="tu_contraseña",
+        host="127.0.0.1",
+        port="5432",
+        database="tu_base_de_datos"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("Conectado a - ", record, "\n")
+except (Exception, psycopg2.Error) as error:
+    print("Error al conectar a PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Conexión a PostgreSQL cerrada")
