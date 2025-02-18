0ae208710856b0ef56ddb63e0ef27ab7f8499f4a
Megaomega99 <125285112+Megaomega99@users.noreply.github.com>
2025-02-18 15:11:55 -0500

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,8 @@
-# db/session.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from core.config import settings
+# app/db/session.py
+from typing import Generator
+from .base import SessionLocal
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
+def get_db() -> Generator:
     db = SessionLocal()
     try:
         yield db
